3cccfe790000eeae2ea4ca5ea9fbb4c1222c37d5
Gabriel Ronei <gabriheron@hotmail.com>
2021-02-16 22:59:41 -0300

    Finalizando aula 10

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,35 @@ import Menu from "../src/components/commons/Menu";
 import { Button } from '../src/components/commons/Button';
 import Text from "../src/components/foundation/Text";
 import { Grid } from "../src/components/layout/Grid";
+import { Box } from "../src/components/layout/Box";
 
 export default function Home() {
 
   return (
-    <div style={{
-      flex: '1',
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-    }}>
+    <Box
+      flex="1"
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="column"
+      justifyContent="space-between"
+      backgroundImage="url(/images/bubbles.svg)"
+      backgroundRepeat="no-repeat"
+      backgroundPosition="bottom right"
+    >
       <Menu />
 
-      <Grid.Container>
+      <Grid.Container marginTop={{
+        xs: '32px',
+        md: '75px',
+      }}>
         <Grid.Row>
           <Grid.Col
-            offset={1}
             value={{ xs: 12, md: 5 }}
+            offset={{ xs: 0, md: 1 }}
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="center"
+            flexDirection="column"
           >
             <div>
               <Text
@@ -58,12 +69,13 @@ export default function Home() {
                 Cadastrar
               </Button>
             </div>
-            </Grid.Col>
+          </Grid.Col>
           <Grid.Col
             value={{ xs: 12, md: 6 }}
           >
             <img
               style={{ display: 'block', margin: 'auto' }}
+              alt="Imagem de celular com páginas internas do projeto com o perfil do Cage"
               src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
             />
           </Grid.Col>
@@ -71,6 +83,6 @@ export default function Home() {
       </Grid.Container>
 
       <Footer />
-    </div>
+    </Box>
   )
 }
